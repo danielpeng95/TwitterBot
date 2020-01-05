ac0900cddb582e46801b51b1720a7e7e57c5ac8c
danielpeng95 <danielpeng95@yahoo.com>
2020-01-05 13:29:22 -0500

    add retweet, post, img

--- tests/twitterBot.js
+++ tests/twitterBot.js
@@ -3,18 +3,24 @@ module.exports = {
     beforeEach: browser => {
         Twitter = browser.page.twitterPageObj()
         Twitter.navigate()
+        .maximizeWindow()
     },
     after: browser => {
         Twitter.end()
     },
     'Retweet': browser => {
         Twitter
-            .waitForElementVisible('@email')
+            .waitForElementVisible('@email', 10000)
             .setValue('@email', 'softwareqa10@yahoo.com')
             .setValue('@password', 'SoftQA1995')
             .click('@submit')
+            // .waitForElementVisible('@like', 10000)
+            // .click('@like')
             .waitForElementVisible('@retweet', 10000)
             .click('@retweet')
+            .waitForElementVisible('@post', 10000)
             .click('@post')
+            .waitForElementVisible('@img', 10000)
+            .click('@img')
     }
 }
