282ca099d9cf2e66805f962e1966e6c795aedfd4
danielpeng95 <danielpeng95@yahoo.com>
2020-01-05 13:28:46 -0500

    adding retweet, post, img

--- pageObjects/twitterPageObj.js
+++ pageObjects/twitterPageObj.js
@@ -5,10 +5,13 @@ module.exports = {
     }],
 
     elements: { //xPath Selector
+
         email: {selector: '//input[@type="text"]', locateStrategy: 'xpath'},
         password: {selector: '//input[@type="password"]', locateStrategy: 'xpath'},
         submit: {selector: '//input[@type="submit"]', locateStrategy: 'xpath'},
-        retweet: {selector: '(//div[@class="css-1dbjc4n r-xoduu5 r-1udh08x"])[2]', locateStrategy: 'xpath'},
+        retweet: {selector: '(//div[@class="css-1dbjc4n r-xoduu5 r-1udh08x"])[1]', locateStrategy: 'xpath'},
+        like: {selector: '(//div[@class="css-1dbjc4n r-xoduu5 r-1udh08x"])[3]', locateStrategy: 'xpath'},
         post: {selector: '(//div[@aria-haspopup="false"])[1]', locateStrategy: 'xpath'},
+        img: {selector: '(//div[@class="css-1dbjc4n r-1awozwy r-sdzlij r-18u37iz r-1777fci r-dnmrzs r-1sp51qo r-o7ynqc r-6416eg"])[7]', locateStrategy: 'xpath'},
     }
 }
