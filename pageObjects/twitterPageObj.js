77c1823b5044d1c2ff50a11060cf891450194b90
danielpeng95 <danielpeng95@yahoo.com>
2020-01-06 18:42:47 -0500

    selenium log

--- pageObjects/twitterPageObj.js
+++ pageObjects/twitterPageObj.js
@@ -12,6 +12,6 @@ module.exports = {
         retweet: {selector: '(//div[@class="css-1dbjc4n r-xoduu5 r-1udh08x"])[1]', locateStrategy: 'xpath'},
         like: {selector: '(//div[@class="css-1dbjc4n r-xoduu5 r-1udh08x"])[3]', locateStrategy: 'xpath'},
         post: {selector: '(//div[@aria-haspopup="false"])[1]', locateStrategy: 'xpath'},
-        img: {selector: '(//div[@class="css-1dbjc4n r-1awozwy r-sdzlij r-18u37iz r-1777fci r-dnmrzs r-1sp51qo r-o7ynqc r-6416eg"])[7]', locateStrategy: 'xpath'},
+        profile: {selector: '(//div[@class="css-1dbjc4n r-1awozwy r-sdzlij r-18u37iz r-1777fci r-dnmrzs r-1sp51qo r-o7ynqc r-6416eg"])[7]', locateStrategy: 'xpath'},
     }
 }
